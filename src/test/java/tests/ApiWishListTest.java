5e58fe90621ef133a3183d38611eb936e52f3d85
Nick Tokarev <tokarev_ns>
2021-04-05 01:42:43 +0300

    add templates

---
+++ src/test/java/tests/ApiWishListTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static restAssured.CustomSpec.spec;
+
+import static org.hamcrest.Matchers.is;
+import static utils.FileUtils.readStringFromFile;
+
+public class ApiWishListTest extends TestBase {
+
+    @Test
+    void addItemToWishlist() {
+        String messageText = readStringFromFile("./src/test/resources/message_text.txt");
+
+        Response response =
+                spec().request()
+                        .when()
+                        .post("/addproducttocart/details/34/2")
+                        .then()
+                        .statusCode(200)
+                        .log().body()
+                        .body("success", is(true))
+                        .body("message", is(messageText))
+                        .extract().response();
+        System.out.println(response);
+    }
+}
